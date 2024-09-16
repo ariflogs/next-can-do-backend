68a34dda8459be21791a0b94261026d746065143
ariflogs <devarifhossain@gmail.com>
2024-09-16 17:23:42 +0600

    zod validatioin ✅

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,17 +1,36 @@
 import { NextRequest, NextResponse } from "next/server";
+import { z, ZodError } from "zod";
+import { prisma } from "../utils/prisma";
 
-const data: any = [];
-
-export function GET() {
-    return NextResponse.json({
-        data
-    })
+export async function GET() {
+  const users = await prisma.user.findMany();
+  return NextResponse.json({
+    users,
+  });
 }
 
+const userCreateSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+});
+
 export async function POST(req: NextRequest) {
+  try {
     const body = await req.json();
-    const headers = req.headers.get("auth")
-    console.log(headers)
-    data.push(body)
-    return NextResponse.json({body})
+    const data = userCreateSchema.parse(body);
+    await prisma.user.create({
+      data: {
+        name: data.name,
+        email: data.email,
+      },
+    });
+    return NextResponse.json({ body });
+  } catch (error) {
+    if (error instanceof ZodError) {
+        const zodErrors = JSON.parse(error.message);
+      return NextResponse.json({ error: zodErrors[0].message }, { status: 400 });
+    }
+
+    return NextResponse.json({ error: "Something went wrong!" }, { status: 500 });
+  }
 }
